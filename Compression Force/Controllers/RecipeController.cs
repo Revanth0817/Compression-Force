22676221ac55931ce60a04d9081a1f5a38308392
jimny90 <venkatesh,palanisamy@kaarinfotech.com>
2025-12-31 19:10:56 +0530

    RecipeController and RecipeParameter brought from local that i already had.

--- Compression Force/Controllers/RecipeController.cs
+++ Compression Force/Controllers/RecipeController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System;
+using System.Collections;
+
 
 namespace Compression_Force.Controllers
 {
@@ -43,9 +45,12 @@ namespace Compression_Force.Controllers
         [HttpGet]
         public IActionResult RecipeParameter()
         {
-            var allCodes = _context.Recipes.Select(r => r.RecipeCode).Distinct().OrderBy(c => c).ToList();
+            var allCodes = _context.Recipes
+                .Select(r => r.RecipeCode)
+                .Distinct()
+                .OrderBy(c => c)
+                .ToList();
 
-            // Get the first recipe in the list as the default display
             var initialCode = allCodes.FirstOrDefault();
             var firstRecipe = _context.Recipes.FirstOrDefault(r => r.RecipeCode == initialCode);
 
@@ -55,18 +60,115 @@ namespace Compression_Force.Controllers
                 SelectedRecipe = firstRecipe
             };
 
+            var dropdownData = BuildDropdownData(firstRecipe);
+            ViewData["toolTypes"] = dropdownData.toolTypes;
+            ViewData["toolTypeDisplay"] = dropdownData.toolTypeDisplay;
+            ViewData["awcList"] = dropdownData.awcList;
+            ViewData["awcDisplay"] = dropdownData.awcDisplay;
+            ViewData["recipeList"] = dropdownData.recipeList;
+            ViewData["recipeDisplay"] = dropdownData.recipeDisplay;
+
+            ViewData["toolTypes"] = dropdownData.toolTypes;
+            ViewData["toolTypeDisplay"] = dropdownData.toolTypeDisplay;
+            ViewData["awcList"] = dropdownData.awcList;
+            ViewData["awcDisplay"] = dropdownData.awcDisplay;
+
+            ViewData["Title"] = "RecipeParameter";
+
             return View(viewModel);
         }
+
 
         [HttpGet]
         public IActionResult GetRecipeDetails(string code)
         {
             var recipe = _context.Recipes.FirstOrDefault(r => r.RecipeCode == code);
-            if (recipe == null) return NotFound();
+            if (recipe == null) return Json(null);
+            var dropdownData = BuildDropdownData(recipe);
+            return Json(new
+            {
+                recipeCode = recipe.RecipeCode,
+                toolTypes = dropdownData.toolTypes,
+                toolTypeDisplay = dropdownData.toolTypeDisplay,
+                awcList = dropdownData.awcList,
+                awcDisplay = dropdownData.awcDisplay,
+                recipeList = dropdownData.recipeList,
+                recipeDisplay = dropdownData.recipeDisplay,
 
-            // Returns the full database row as a JSON object
-            return Json(recipe);
+
+                // Example product parameters (add all you need)
+                forceFeederRatioS1 = dropdownData.forceFeederRatioS1,
+                forceFeederRatioS1Display = dropdownData.forceFeederRatioS1Display,
+                fillDepthS1 = recipe.FillDepthS1,
+                mainPenetrationPositionS1 = recipe.MainPenetrationPositionS1,
+                mainThicknessPositionS1 = recipe.MainThicknessPositionS1,
+                prePenetrationPositionS1 = recipe.PrePenetrationPositionS1,
+                preThicknessPositionS1 = recipe.PreThicknessPositionS1,
+                sampleIntervalS1 = recipe.SampleIntervalS1,
+                sampleRevolutionQtyS1 = recipe.SampleRevolutionQtyS1,
+                maxMainCompForceS1 = recipe.MaxMainCompForceS1,
+                maxPreCompForceS1 = recipe.MaxPreCompForceS1,
+                maxEjectionForceS1 = recipe.MaxEjectionForceS1,
+                // AWC & AR parameters
+                rejectionForceLimitMaxS1 = recipe.RejectionForceLimitMaxS1,
+                awcForceLimitMaxS1 = recipe.AwcForceLimitMaxS1,
+                awcForceSetPointS1 = recipe.AwcForceSetPointS1,
+                awcForceLimitMinS1 = recipe.AwcForceLimitMinS1,
+                rejectionForceLimitMinS1 = recipe.RejectionForceLimitMinS1,
+
+                // Example product parameters (add all you need)
+                forceFeederRatioS2 = dropdownData.forceFeederRatioS2,
+                forceFeederRatioS2Display = dropdownData.forceFeederRatioS2Display,
+                fillDepthS2 = recipe.FillDepthS2,
+                mainPenetrationPositionS2 = recipe.MainPenetrationPositionS2,
+                mainThicknessPositionS2 = recipe.MainThicknessPositionS2,
+                prePenetrationPositionS2 = recipe.PrePenetrationPositionS2,
+                preThicknessPositionS2 = recipe.PreThicknessPositionS2,
+                sampleIntervalS2 = recipe.SampleIntervalS2,
+                sampleRevolutionQtyS2 = recipe.SampleRevolutionQtyS2,
+                maxMainCompForceS2 = recipe.MaxMainCompForceS2,
+                maxPreCompForceS2 = recipe.MaxPreCompForceS2,
+                maxEjectionForceS2 = recipe.MaxEjectionForceS2,
+                // AWC & AR parameters
+                rejectionForceLimitMaxS2 = recipe.RejectionForceLimitMaxS2,
+                awcForceLimitMaxS2 = recipe.AwcForceLimitMaxS2,
+                awcForceSetPointS2 = recipe.AwcForceSetPointS2,
+                awcForceLimitMinS2 = recipe.AwcForceLimitMinS2,
+                rejectionForceLimitMinS2 = recipe.RejectionForceLimitMinS2
+            });
         }
+
+        private (IEnumerable<string> toolTypes, IEnumerable<string> awcList, IEnumerable<string> recipeList, IEnumerable<string> forceFeederRatioS1, IEnumerable<string> forceFeederRatioS2, string toolTypeDisplay, string awcDisplay, string recipeDisplay, string forceFeederRatioS1Display, string forceFeederRatioS2Display)
+            BuildDropdownData(Recipe recipe)
+        {
+            IEnumerable<string> GetItems(object val)
+            {
+                if (val == null) return new List<string> { "" };
+                if (val is string s) return new List<string> { s };
+                if (val is IEnumerable enumerable)
+                {
+                    var list = new List<string>();
+                    foreach (var item in enumerable) list.Add(item?.ToString() ?? "");
+                    return list;
+                }
+                return new List<string> { val.ToString() };
+            }
+
+            var toolTypes = GetItems(recipe?.ToolType);
+            var awcList = GetItems(new[] { "Item1", "1111" });
+            var recipeList = GetItems(recipe?.RecipeName);
+            var forceFeederRatioS1 = GetItems(new[] { "0.5", "1.0", "2.0" });
+            var forceFeederRatioS2 = GetItems(new[] { "0.5", "1.0" });
+
+            return (toolTypes, awcList, recipeList, forceFeederRatioS1, forceFeederRatioS2,
+                    toolTypes.FirstOrDefault() ?? "",
+                    awcList.FirstOrDefault() ?? "",
+                    recipeList.FirstOrDefault() ?? "",
+                    forceFeederRatioS1.FirstOrDefault() ?? "",
+                    forceFeederRatioS2.FirstOrDefault() ?? "");
+        }
+
+
 
         [HttpGet]
         public IActionResult CheckCodeExists(string code)
@@ -101,44 +203,44 @@ namespace Compression_Force.Controllers
             return RedirectToAction("AddRecipe");
         }
 
-/*
-        [HttpGet]
-        public IActionResult AddRecipe()
-        {
-            var code = TempData["NewRecipeCode"] as string;
+        /*
+                [HttpGet]
+                public IActionResult AddRecipe()
+                {
+                    var code = TempData["NewRecipeCode"] as string;
 
-            if (string.IsNullOrEmpty(code))
-            {
-                return RedirectToAction("RecipeParameter");
-            }
+                    if (string.IsNullOrEmpty(code))
+                    {
+                        return RedirectToAction("RecipeParameter");
+                    }
 
-            // Initialize model with the code and current timestamp
-            var model = new Recipe
-            {
-                RecipeCode = code,
-                DateTime = DateTime.Now
-            };
+                    // Initialize model with the code and current timestamp
+                    var model = new Recipe
+                    {
+                        RecipeCode = code,
+                        DateTime = DateTime.Now
+                    };
 
-            return View(model);
-        }
-*/
-       /* [HttpGet]
-        public IActionResult AddRecipe(string code)
-        {
-            if (string.IsNullOrEmpty(code))
-            {
-                return RedirectToAction("RecipeParameter");
-            }
+                    return View(model);
+                }
+        */
+        /* [HttpGet]
+         public IActionResult AddRecipe(string code)
+         {
+             if (string.IsNullOrEmpty(code))
+             {
+                 return RedirectToAction("RecipeParameter");
+             }
 
-            // Initialize the model with the code provided from the modal
-            var model = new Recipe
-            {
-                RecipeCode = code,
-                DateTime = DateTime.Now
-            };
+             // Initialize the model with the code provided from the modal
+             var model = new Recipe
+             {
+                 RecipeCode = code,
+                 DateTime = DateTime.Now
+             };
 
-            return View(model);
-        }*/
+             return View(model);
+         }*/
 
         [HttpPost]
         public IActionResult SaveNewRecipe(Recipe model)
